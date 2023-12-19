10b2799b7470a70ffbaa53dbcf354dcafc270934
Md. Jahid Hasan <jahid.csedu@gmail.com>
2023-12-19 21:36:32 +0900

    Project created

---
+++ src/main/java/com/example/bookloader/BookLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookloader/BookLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
